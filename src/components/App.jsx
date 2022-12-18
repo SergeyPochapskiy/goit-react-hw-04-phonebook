20d6e918e3f46ffa275cde345153951345ece7b2
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2022-12-18 13:45:06 +0200

    fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,10 @@ import { ContactsList } from './ContactsList/ContactsList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Wrap } from './App.styled';
 
-export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    return (
-      contacts ?? [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
-  });
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
 
   const [filter, setFilter] = useState('');
 
@@ -25,11 +17,11 @@ export default function App() {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-  useEffect(() => {
-    return () => {
-      localStorage.removeItem('contacts');
-    };
-  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     localStorage.removeItem('contacts');
+  //   };
+  // }, []);
 
   function addContact({ name, number }) {
     if (contacts.find(contact => contact.name === name)) {
